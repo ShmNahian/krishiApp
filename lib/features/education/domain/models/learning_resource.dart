2c1187bf75605745659340100cafb4b18713da77
Shamsul huda Md Nahian <114670807+ShmNahian@users.noreply.github.com>
2026-01-31 15:14:25 +0600

    Add files via upload

---
+++ lib/features/education/domain/models/learning_resource.dart
@@ -0,0 +1,8 @@
+class LearningResource {
+  LearningResource(
+      {required this.title, required this.type, required this.link});
+
+  final String title;
+  final String type; // article or video
+  final String link;
+}
