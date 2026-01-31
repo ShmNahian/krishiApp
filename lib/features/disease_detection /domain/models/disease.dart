b9919f46e24a987a76d35772762265215ba3af4b
Shamsul huda Md Nahian <114670807+ShmNahian@users.noreply.github.com>
2026-01-31 15:06:04 +0600

    Add files via upload

---
+++ lib/features/disease_detection /domain/models/disease.dart
@@ -0,0 +1,8 @@
+class Disease {
+  Disease(
+      {required this.name, required this.description, required this.imageUrl});
+
+  final String name;
+  final String description;
+  final String imageUrl;
+}
