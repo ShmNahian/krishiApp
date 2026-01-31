6b5255274d46b344cc33af3e05b3f8b0b9617264
Shamsul huda Md Nahian <114670807+ShmNahian@users.noreply.github.com>
2026-01-31 15:35:32 +0600

    Add files via upload

---
+++ lib/features/weather/domain/models/weather_info.dart
@@ -0,0 +1,10 @@
+class WeatherInfo {
+  WeatherInfo(
+      {required this.temperatureC,
+      required this.rainChance,
+      required this.condition});
+
+  final double temperatureC;
+  final double rainChance;
+  final String condition;
+}
