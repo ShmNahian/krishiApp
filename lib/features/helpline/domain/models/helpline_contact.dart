330347aa3cd149381d3cefa748f99bb5bf4ad84b
Shamsul huda Md Nahian <114670807+ShmNahian@users.noreply.github.com>
2026-01-31 15:19:33 +0600

    Add files via upload

---
+++ lib/features/helpline/domain/models/helpline_contact.dart
@@ -0,0 +1,8 @@
+class HelplineContact {
+  HelplineContact(
+      {required this.name, required this.phone, required this.region});
+
+  final String name;
+  final String phone;
+  final String region;
+}
